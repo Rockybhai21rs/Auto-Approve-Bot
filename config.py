9957610ae4240518e4d04030eb3daf5e24f03451
Rockybhai21rs <rockingstar.21rs@gmail.com>
2025-04-04 12:22:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "6681cd17e17fb536d00bfad2bd9f0cd6"))
 API_HASH = environ.get("API_HASH", "22193332")
-BOT_TOKEN = environ.get("BOT_TOKEN", "7509459901:AAHLb7yGn0YhHasiOVM8UdFioeZX0v9gnFY")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7783239593:AAHYcCcUsof4PTxzUnljueiRocDqNVZyqqM")
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002206934353"))
 ADMINS = int(environ.get("ADMINS", "6947378236"))
 DB_URI = environ.get("DB_URI", "mongodb+srv://beanonymoushacker09:q9gxQwIZDCmVOycI@cluster0.ej0gk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
